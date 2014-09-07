860834df329b1f530f692ab17eb081a825b906d9
Bastian Alejandro Muhlhauser Aravena <b.muhlhausera@gmail.com>
2014-09-07 17:09:40 -0300

    IA 2

---
+++ El Despertar De Visuc/Assets/Scripts/Enemy_2.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+private var objetivo : Transform;
+private var movVelocidad : int = 3;
+private var rotationSpeed = 3;
+public var animator : Animator;
+private var miTransform : Transform;
+private var tempScale : Transform;
+private var player : GameObject;
+
+function Awake(){
+	miTransform = transform;
+
+}
+
+function Start () {
+	player = GameObject.FindGameObjectWithTag("Player");
+	objetivo = GameObject.FindGameObjectWithTag("Player").transform;
+	animator = GetComponent(Animator);
+	tempScale = miTransform;
+}
+
+function FixedUpdate () {
+	if(animator.GetBool("Tocado")){
+		animator.SetBool("moverZombie", false);
+		
+	}
+	Mirar();
+	if(transform.position.x != objetivo.position.x){
+     	animator.SetBool("moverZombie", true);
+    }else{
+    	animator.SetBool("moverZombie", false);
+    }
+	miTransform.rotation = Quaternion.Slerp(miTransform.rotation, Quaternion.LookRotation(objetivo.position - miTransform.position), rotationSpeed*Time.deltaTime);
+	miTransform.position += miTransform.forward * movVelocidad * Time.deltaTime;	
+	miTransform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
+	
+}
+
+function Mirar(){
+	if(player.rigidbody2D.velocity.x != 0){
+	if(tempScale.localScale.x != objetivo.localScale.x){
+		if(objetivo.localScale.x > 0){//si el jugador esta mirando a la izquierda(aca lo comprueba por la escala y si esta mirando a la izquierda lo voltea a la derecha)
+			transform.localScale.x *= -1;
+		}else{
+			transform.localScale.x *= 1;
+		}
+		tempScale.localScale.x = objetivo.localScale.x; 
+	}	
+	}
+	
+}
