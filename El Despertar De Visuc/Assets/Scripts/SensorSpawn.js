2d36b435e28ada14d09033175af684bea544a8fd
Bastian Alejandro Muhlhauser Aravena <b.muhlhausera@gmail.com>
2014-09-09 22:30:24 -0300

    update

---
+++ El Despertar De Visuc/Assets/Scripts/SensorSpawn.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+private var spawn : Spawn;
+
+function Start () {
+	var temp : GameObject = GameObject.FindWithTag("spawner");
+	spawn = temp.gameObject.GetComponent(Spawn);
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(colision : Collider2D){
+	if(colision.tag == "Player"){
+		spawn.setSpawn(true);
+	}
+}
---
+++ El Despertar De Visuc/Assets/Scripts/Spawn.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+public var prefab : GameObject; 
+public var cantidadAGenerar : int; 
+public var velocidadDeGeneracion : int; 
+private var rotacion : Quaternion = Quaternion.identity; 
+private var numerodeInstancias:int = 0; 
+private var tiempoDeGeneracion : float; 
+public var SensorActivado : boolean;
+
+function Start(){
+	SensorActivado = false;
+}
+function Update () { 
+	if(SensorActivado == true){
+	    if((numerodeInstancias <= cantidadAGenerar) && (Time.time > tiempoDeGeneracion)) 
+	    { 
+	        numerodeInstancias += 1; 
+	        tiempoDeGeneracion = Time.time + velocidadDeGeneracion; 
+	        var objeto:GameObject = Instantiate(prefab,gameObject.transform.position,rotacion); 
+	    } 
+	}
+} 
+
+function setSpawn(estado : boolean){
+	
+	this.SensorActivado = estado;
+	
+}
